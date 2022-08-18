2d93f6f4548288c14a199f91986a0a9bf3a8498a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2022-08-18 07:32:58 -0500

    Add docstring to all functions

--- dsfromscratch/4.linear_algebra/chapter_examples.py
+++ dsfromscratch/4.linear_algebra/chapter_examples.py
@@ -9,6 +9,7 @@ grades = [95, 80, 75, 62]
 
 
 def add(v: Vector, w: Vector) -> Vector:
+    """Adds corresponding elements"""
     assert len(v) == len(w), "Vectors must be the same length"
 
     return [v_i + w_i for v_i, w_i in zip(v, w)]
@@ -18,6 +19,7 @@ assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
 
 
 def subtract(v: Vector, w: Vector) -> Vector:
+    """Subtracts corresponding elements"""
     assert len(v) == len(w), "Vectors must be the same length"
 
     return [v_i - w_i for v_i, w_i in zip(v, w)]
@@ -27,6 +29,7 @@ assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
 
 
 def vector_sum(vectors: List[Vector]) -> Vector:
+    """Sums all corresponding elements"""
     assert vectors
 
     num_elements = len(vectors[0])
@@ -39,6 +42,7 @@ assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
 
 
 def scalar_mulitply(c: float, v: Vector) -> Vector:
+    """Multiples every element by c"""
     return [c * v_i for v_i in v]
 
 
@@ -46,6 +50,7 @@ assert scalar_mulitply(2, [1, 2, 3]) == [2, 4, 6]
 
 
 def vector_mean(vectors: List[Vector]) -> Vector:
+    """Computes the element-wise average"""
     n = len(vectors)
     return scalar_mulitply(1 / n, vector_sum(vectors))
 
@@ -54,6 +59,10 @@ assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
 
 
 def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 + w_1 + ... + v_n * w_n, ie Dot Product
+
+    Projection of one vector onto another
+    """
     assert len(v) == len(w), "vectors must be the same length"
 
     return sum(v_i * w_i for v_i, w_i in zip(v, w))
@@ -63,6 +72,7 @@ assert dot([1, 2, 3], [4, 5, 6]) == 32
 
 
 def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
     return dot(v, v)
 
 
@@ -70,6 +80,7 @@ assert sum_of_squares([1, 2, 3]) == 14
 
 
 def magnitude(v: Vector) -> float:
+    """Returns the magnitude (or length) of v"""
     return math.sqrt(sum_of_squares(v))
 
 
@@ -77,10 +88,12 @@ assert magnitude([3, 4]) == 5
 
 
 def squared_distance(v: Vector, w: Vector) -> float:
+    """Computes (v_1 - w_1) ** 2 + ... (v_n - w_n) ** 2"""
     return sum_of_squares(subtract(v, w))
 
 
 def distance(v: Vector, w: Vector) -> float:
+    """Computes the distances between v and w"""
     return magnitude(subtract(v, w))
 
 
@@ -88,6 +101,7 @@ Matrix = List[List[float]]
 
 
 def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns (# of rows of A, # of columns of A)"""
     num_rows = len(A)
     num_columns = len(A[0]) if A else 0
     return num_rows, num_columns
@@ -97,10 +111,12 @@ assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
 
 
 def get_row(A: Matrix, i: int) -> Vector:
+    """Returns the i-th row of A (as a Vector)"""
     return A[i]
 
 
 def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A (as a Vector)"""
     return [A_i[j] for A_i in A]
 
 
@@ -109,10 +125,15 @@ def make_matrix(
     num_cols: int,
     entry_fn: Callable[[int, int], float],
 ) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)=th entry is entry_fn(i,j)
+    """
     return [[entry_fn(i, j) for j in range(num_cols)] for i in range(num_rows)]
 
 
 def identity_matrix(n: int) -> Matrix:
+    """Returns the nx n identity matrix"""
     return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
 
 
