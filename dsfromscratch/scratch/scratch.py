8ea75e47b8f5bde8c89064f768e119efe7de8c0d
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2022-08-25 07:13:38 -0500

    Add chapter 6 methods to scratch module

--- dsfromscratch/scratch/scratch.py
+++ dsfromscratch/scratch/scratch.py
@@ -1,4 +1,5 @@
 import math
+import random
 from typing import List, Tuple, Callable
 from collections import Counter
 
@@ -233,3 +234,58 @@ def correlation(xs: List[float], ys: List[float]) -> float:
         return covariance(xs, ys) / stdev_x / stdev_y
     else:
         return 0
+
+
+def uniform_pdf(x: float) -> float:
+    return 1 if 0 <= x < 1 else 0
+
+
+def uniform_cdf(x: float) -> float:
+    """Returns the probability that a unifrom random variable is <= x"""
+    # fmt: off
+    if x < 0: return 0
+    elif x < 1: return x
+    else: return 1
+    # fmt: on
+
+
+SQRT_TWO_PIE = math.sqrt(2 * math.pi)
+
+
+def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (math.exp(-((x - mu) ** 2) / 2 / sigma ** 2)) / (SQRT_TWO_PIE * sigma)
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+
+def inverse_normal_cdf(
+    p: float, mu: float = 0, sigma: float = 0, tolerance: float = 0.00001
+) -> float:
+    """Find approximate inverse using binary search"""
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+
+    low_z = -10.0
+    hi_z = 10
+
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z = mid_z
+        else:
+            hi_z = mid_z
+
+    return mid_z
+
+
+def bernoulli_trial(p: float) -> int:
+    """Returns 1 with probability p and 0 with probability 1-p"""
+    return 1 if random.random() < p else 0
+
+
+def binomial(n: int, p: float) -> int:
+    """Returns the sum of n bernoulli(p) trials"""
+    return sum(bernoulli_trial(p) for _ in range(n))
